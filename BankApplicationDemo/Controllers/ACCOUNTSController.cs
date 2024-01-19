733afe8e5670b06d3eecedc7dfc825dc0ed1d7d4
harshvad14 <157087764+harshvad14@users.noreply.github.com>
2024-01-19 09:20:20 +0530

    initial commit

---
+++ BankApplicationDemo/Controllers/ACCOUNTSController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BankApplicationDemo;
+
+namespace BankApplicationDemo.Controllers
+{
+    public class ACCOUNTSController : Controller
+    {
+        private BankingAppContext db = new BankingAppContext();
+
+        // GET: ACCOUNTS
+        public ActionResult Index()
+        {
+            return View(db.T_ACCOUNTS.ToList());
+        }
+
+        // GET: ACCOUNTS/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            T_ACCOUNTS t_ACCOUNTS = db.T_ACCOUNTS.Find(id);
+            if (t_ACCOUNTS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(t_ACCOUNTS);
+        }
+
+        // GET: ACCOUNTS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ACCOUNTS/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AccountID,AccountName,AccountBalance")] T_ACCOUNTS t_ACCOUNTS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.T_ACCOUNTS.Add(t_ACCOUNTS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(t_ACCOUNTS);
+        }
+
+        // GET: ACCOUNTS/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            T_ACCOUNTS t_ACCOUNTS = db.T_ACCOUNTS.Find(id);
+            if (t_ACCOUNTS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(t_ACCOUNTS);
+        }
+
+        // POST: ACCOUNTS/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AccountID,AccountName,AccountBalance")] T_ACCOUNTS t_ACCOUNTS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(t_ACCOUNTS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(t_ACCOUNTS);
+        }
+
+        // GET: ACCOUNTS/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            T_ACCOUNTS t_ACCOUNTS = db.T_ACCOUNTS.Find(id);
+            if (t_ACCOUNTS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(t_ACCOUNTS);
+        }
+
+        // POST: ACCOUNTS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            T_ACCOUNTS t_ACCOUNTS = db.T_ACCOUNTS.Find(id);
+            db.T_ACCOUNTS.Remove(t_ACCOUNTS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BankApplicationDemo/Controllers/TRANSACTIONSController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BankApplicationDemo;
+
+namespace BankApplicationDemo.Controllers
+{
+    public class TRANSACTIONSController : Controller
+    {
+        private BankingAppContext db = new BankingAppContext();
+
+        // GET: TRANSACTIONS
+        public ActionResult Index()
+        {
+            var t_TRANSACTIONS = db.T_TRANSACTIONS.Include(t => t.T_ACCOUNTS).Include(t => t.T_ACCOUNTS1);
+            return View(t_TRANSACTIONS.ToList());
+        }
+
+        // GET: TRANSACTIONS/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            T_TRANSACTIONS t_TRANSACTIONS = db.T_TRANSACTIONS.Find(id);
+            if (t_TRANSACTIONS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(t_TRANSACTIONS);
+        }
+
+        // GET: TRANSACTIONS/Create
+        public ActionResult Create()
+        {
+            ViewBag.FromAccountID = new SelectList(db.T_ACCOUNTS, "AccountID", "AccountName");
+            ViewBag.ToAccountID = new SelectList(db.T_ACCOUNTS, "AccountID", "AccountName");
+            return View();
+        }
+
+        // POST: TRANSACTIONS/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FromAccountID,ToAccountID,TransactionTime,TransactionAmout,FromAccountBalance,ToAccountBalance,ID")] T_TRANSACTIONS t_TRANSACTIONS)
+        {
+            if (ModelState.IsValid)
+            {
+                var fromAccount = db.T_ACCOUNTS.Find(t_TRANSACTIONS.FromAccountID);
+                var toAccount = db.T_ACCOUNTS.Find(t_TRANSACTIONS.ToAccountID);
+                var amount = t_TRANSACTIONS.TransactionAmout;
+                t_TRANSACTIONS.FromAccountBalance = fromAccount.AccountBalance - amount;
+                t_TRANSACTIONS.ToAccountBalance = toAccount.AccountBalance + amount;
+                t_TRANSACTIONS.TransactionTime = DateTime.Now;
+                if(fromAccount.AccountBalance<=0 || fromAccount.AccountBalance < amount || t_TRANSACTIONS.ToAccountID == t_TRANSACTIONS.FromAccountID)
+                {
+                    return RedirectToAction("Index");
+                }
+                
+                db.T_TRANSACTIONS.Add(t_TRANSACTIONS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FromAccountID = new SelectList(db.T_ACCOUNTS, "AccountID", "AccountName", t_TRANSACTIONS.FromAccountID);
+            ViewBag.ToAccountID = new SelectList(db.T_ACCOUNTS, "AccountID", "AccountName", t_TRANSACTIONS.ToAccountID);
+            return View(t_TRANSACTIONS);
+        }
+
+        // GET: TRANSACTIONS/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            T_TRANSACTIONS t_TRANSACTIONS = db.T_TRANSACTIONS.Find(id);
+            if (t_TRANSACTIONS == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FromAccountID = new SelectList(db.T_ACCOUNTS, "AccountID", "AccountName", t_TRANSACTIONS.FromAccountID);
+            ViewBag.ToAccountID = new SelectList(db.T_ACCOUNTS, "AccountID", "AccountName", t_TRANSACTIONS.ToAccountID);
+            return View(t_TRANSACTIONS);
+        }
+
+        // POST: TRANSACTIONS/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FromAccountID,ToAccountID,TransactionTime,TransactionAmout,FromAccountBalance,ToAccountBalance,ID")] T_TRANSACTIONS t_TRANSACTIONS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(t_TRANSACTIONS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FromAccountID = new SelectList(db.T_ACCOUNTS, "AccountID", "AccountName", t_TRANSACTIONS.FromAccountID);
+            ViewBag.ToAccountID = new SelectList(db.T_ACCOUNTS, "AccountID", "AccountName", t_TRANSACTIONS.ToAccountID);
+            return View(t_TRANSACTIONS);
+        }
+
+        // GET: TRANSACTIONS/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            T_TRANSACTIONS t_TRANSACTIONS = db.T_TRANSACTIONS.Find(id);
+            if (t_TRANSACTIONS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(t_TRANSACTIONS);
+        }
+
+        // POST: TRANSACTIONS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            T_TRANSACTIONS t_TRANSACTIONS = db.T_TRANSACTIONS.Find(id);
+            db.T_TRANSACTIONS.Remove(t_TRANSACTIONS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
